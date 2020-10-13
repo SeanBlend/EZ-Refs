4dcad8006dd8429276a67494be30ee14006a388e
Patrick <patrick@patrick.com>
2020-10-12 20:09:46 -0700

    Edit: Not anything big.

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 import pygame
 
 WIDTH, HEIGHT = 800, 600
-DISPLAY = pygame.display.set_mode((WIDTH, HEIGHT))
+DISPLAY = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
