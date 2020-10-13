55b074123bea5a701df5fcb7c44859b297365ace
Patrick <patrick@patrick.com>
2020-10-12 18:52:12 -0700

    File: Added file and added basic constants

---
+++ constants.py
@@ -0,0 +1,9 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 600
+DISPLAY = pygame.display.set_mode((WIDTH, HEIGHT))
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
