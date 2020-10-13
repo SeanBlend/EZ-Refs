810a457a48990a4115f4c95f72522ae4cbcb3915
Patrick <patrick@patrick.com>
2020-10-12 19:20:48 -0700

    Edit: Added Canvas class and other.

--- classes.py
+++ classes.py
@@ -175,3 +175,86 @@ class TextInput:
 
     def __repr__(self):
         return self.input_string
+
+class Button:
+    def __init__(self, loc, size, font, text, bgCol, textCol, borderThickness, borderColor):
+        """
+        Creates a button which you can draw on the screen and check for clicks.
+
+        :param loc: the x, y position of the button on the screen
+        :param size: the width and height of the button
+        :param font: a pygame.font.SysFont() instance
+        :param text: the text that you want the button to display
+        :param bgCol: the color of the button
+        :param textCol: the color of the text you want to display
+        :param borderThickness: the thickness of the border surrounding the button
+        :param borderColor: the color of the border surrounding the button
+        """
+        self.loc, self.size = loc, size
+        self.font = font
+        self.text = text
+        self.bgCol, self.textCol = bgCol, textCol
+        self.thickness, self.borderColor = borderThickness, borderColor
+        self.surf = font.render(text, 1, textCol)
+        self.rect = pygame.Rect(*loc, *size)
+
+    def ChangeText(self, newText):
+        """
+        Changes the text that will be displayed on top of the button
+        :param newText: the new text that you want to change the current text to.
+        :return: None
+        """
+        self.text = newText
+        self.surf = self.font.render(self.text, 1, self.textCol)
+
+    def ChangeTextColor(self, newTextColor):
+        """
+        Changes the color of the text
+        :param newTextColor: the new color of the text.
+        :return: None
+        """
+        self.textCol = newTextColor
+        self.surf = self.font.render(self.text, 1, self.textCol)
+
+    def ChangeBgColor(self, newBgColor):
+        """
+        Changes the background color of the button
+        :param newBgColor: the new background color of the button.
+        :return: None
+        """
+        self.bgCol = newBgColor
+
+    def ChangeBorderColor(self, newBorderColor):
+        """
+        Changes the border color of the button
+        :param newBorderColor: the new border color of the button.
+        :return: None
+        """
+        self.borderColor = newBorderColor
+    
+    def ChangeBorderThickness(self, newBorderThickness):
+        """
+        Changes the thickness of the border
+        :param newBorderThickenss: the new thickness of the border of the button.
+        :return: None
+        """
+        self.thickness = newBorderThickness
+
+    def Draw(self, window):
+        """
+        Draws the button onto the give surface
+        :param window: the surface you want to draw the button to.
+        :return: None
+        """
+        pygame.draw.rect(window, self.bgCol, self.rect)
+        pygame.draw.rect(window, self.borderColor, self.rect, self.thickness)
+        window.blit(self.surf, (int(self.loc[0] + self.size[0]/2 - self.surf.get_width()/2), int(self.loc[1] + self.size[1]/2 - self.surf.get_height()/2)))
+
+    def Clicked(self):
+        """
+        Checks if the button is clicked
+        :return: bool value indicating whether or not the button is clicked
+        """
+        return self.rect.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]
+
+class Canvas:
